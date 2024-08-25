369efcaf3edc3d4a1b0cef85e09b8bc8a807d5e4
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2024-08-25 23:54:20 +0530

    respnse

--- src/components/pages/FormModel.js
+++ src/components/pages/FormModel.js
@@ -1,34 +1,40 @@
-import React from 'react'
-import "../styles/LandingPage.css";
-import { BiCustomize, BiUndo } from 'react-icons/bi';
-import { FaRegEye } from 'react-icons/fa';
-import { IconContext } from 'react-icons';
-import { SiGoogleforms } from 'react-icons/si';
-import { LiaRedoSolid, LiaUndoSolid } from 'react-icons/lia';
-import { CiMenuKebab } from 'react-icons/ci';
+import React from "react";
+import "../styles/Form.css";
+import { BiCustomize, BiUndo } from "react-icons/bi";
+import { FaRegEye, FaRegFolder } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { SiGoogleforms } from "react-icons/si";
+import { LiaRedoSolid, LiaUndoSolid } from "react-icons/lia";
+import { CiMenuKebab } from "react-icons/ci";
+import { IoIosStarOutline } from "react-icons/io";
 const FormModel = () => {
   return (
     <div>
-    <div create-form-header-div>
+      <div create-form-header-div>
         <div>
-        <IconContext.Provider value={{ color: '#7248B9', size: '40px' }}>
-<SiGoogleforms/>
-</IconContext.Provider>
-<p>Untilted Form</p>  
-</div>
-<div>
-<BiCustomize />
-<FaRegEye />
-<LiaUndoSolid />
-<LiaRedoSolid />
-<button>Send</button>
-<CiMenuKebab />
-{/* <LuRedo2 /> */}
-</div>
-      
+          <IconContext.Provider value={{ color: "#7248B9", size: "40px" }}>
+            <SiGoogleforms />
+          </IconContext.Provider>
+          <p>Untilted Form</p>
+          <FaRegFolder />
+          <IoIosStarOutline />
+        </div>
+        <div>
+          <BiCustomize />
+          <FaRegEye />
+          <LiaUndoSolid />
+          <LiaRedoSolid />
+          <button>Send</button>
+          <CiMenuKebab />
+        </div>
+      </div>
+      <div>
+        <p>Questions</p>
+        <p>Responses</p>
+        <p>Settings</p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default FormModel
+export default FormModel;
---
+++ src/components/pages/Responses.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Responses = () => {
+  return (
+    <div>
+       <h1>0  Responses</h1>
+       <div>
+        Accepting Responses
+       </div>
+       <div>Wait for Responses</div>
+    </div>
+  )
+}
+
+export default Responses
